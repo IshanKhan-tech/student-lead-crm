9825978f0dd52e15a0062c4ac35d4b19e62af2cf
Ishan Khan <ishanik6666@gmail.com>
2026-07-28 20:12:42 +0530

    setup single deployment

--- cmd/backend/server.js
+++ cmd/backend/server.js
@@ -14,9 +14,14 @@ const app = express();
 const multer = require("multer");
 const XLSX = require("xlsx");
 
+const path = require("path");
+
 app.use(cors());
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 const upload = multer({
   dest: "uploads/",
@@ -732,6 +737,9 @@ app.post(
 );
 
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
--- cmd/vite.config.js
+++ cmd/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-
   plugins: [react(), tailwindcss()],
 
+  build: {
+    outDir: "backend/public",
+    emptyOutDir: true,
+  },
+
   server: {
-
-    historyApiFallback: true
-
-  }
-
-})
+    historyApiFallback: true,
+  },
+});
